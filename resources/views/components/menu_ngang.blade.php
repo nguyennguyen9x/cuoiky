8e13941d42f7172c8435c22be79e47c1d8ed6378
nguyennguyen9x <54037230+nguyennguyen9x@users.noreply.github.com>
2021-12-02 20:42:19 +0700

    init

---
+++ resources/views/components/menu_ngang.blade.php
@@ -0,0 +1,44 @@
+<div class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
+    <a href="" class="ml-4 text-sm text-gray-700 dark:text-gray-500 underline">Home</a>
+    <a href="" class="ml-4 text-sm text-gray-700 dark:text-gray-500 underline">Log in</a>
+
+    <a href="" class="ml-4 text-sm text-gray-700 dark:text-gray-500 underline">Register</a>
+</div>
+<div class="hidden fixed top-0 left-0 px-6 py-4 sm:block">
+    <nav class="navbar navbar-expand-md navbar-dark bg-dark">
+        <a class="navbar-brand" href="#">Lar.vn</a>
+        <button class="navbar-toggler p-0 border-0" type="button" data-toggle="offcanvas">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="navbar-collapse offcanvas-collapse">
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item active">
+                    <a class="nav-link" href="#">Trang chủ <span class="sr-only">(current)</span></a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Giới thiệu</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Dự án</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Dịch vụ</a>
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="http://example.com" id="dropdown01" data-toggle="dropdown"
+                        aria-haspopup="true" aria-expanded="false">Sản phẩm</a>
+                    <div class="dropdown-menu" aria-labelledby="dropdown01">
+                        <a class="dropdown-item" href="#">PHP</a>
+                        <a class="dropdown-item" href="#">Laravel</a>
+                        <a class="dropdown-item" href="#">Phalcon</a>
+                    </div>
+                </li>
+            </ul>
+            <form class="form-inline my-2 my-lg-0">
+                <input class="form-control mr-sm-2" type="text" placeholder="..." aria-label="Search">
+                <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Tìm kiếm</button>
+            </form>
+        </div>
+    </nav>
+</div>
