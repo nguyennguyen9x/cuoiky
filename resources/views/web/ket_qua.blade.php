9c1d0f8324472a49b7a790134fda16476efba63a
nguyennguyen9x <54037230+nguyennguyen9x@users.noreply.github.com>
2021-12-23 20:52:12 +0700

    tinh diem

---
+++ resources/views/web/ket_qua.blade.php
@@ -0,0 +1,10 @@
+<x-head></x-head>
+<div>
+    <nav class="main">
+        <x-main_left></x-main_left>
+        <div class="column middle">
+            <h2>kết quả bài làm : {{$diem}} </h2>
+        </div>
+    </nav>
+</div>
+<x-footer></x-footer>
